b76aaeb5f7c2c49025c50ece4be24ad4afa8d0a4
Akhila Ficel <akhilaficel@gmail.com>
2020-07-05 20:44:49 +0100

    Moved config details to config.py

--- dailyfeed/config.py
+++ dailyfeed/config.py
@@ -1,9 +1,13 @@
 import os
 
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '666b601b6739add4b3c04df94d9fe4f1'
+
+    db_path = os.path.join(os.path.dirname(__file__), 'app.db')
+    db_uri = 'sqlite:///{}'.format(db_path)
+    SQLALCHEMY_DATABASE_URI = db_uri
+
     MAIL_SERVER = 'smtp-mail.outlook.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
