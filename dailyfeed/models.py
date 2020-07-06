761ee8054e7dabad70b71bd428c8c26d1cece9c8
Akhila Ficel <akhilaficel@gmail.com>
2020-07-06 20:43:38 +0100

    Added User model

--- dailyfeed/models.py
+++ dailyfeed/models.py
@@ -34,3 +34,13 @@ class Article(db.Model):
 
     def __repr__(self):
         return f"Article('{self.title}', '{self.link}')"
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
