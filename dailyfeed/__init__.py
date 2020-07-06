531ad909e494affbf700eec893e61687081a0c18
Akhila Ficel <akhilaficel@gmail.com>
2020-07-06 20:13:14 +0100

    Changes to config file, fixed footer

--- dailyfeed/__init__.py
+++ dailyfeed/__init__.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dailyfeed.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from dailyfeed import routes
+from dailyfeed import routes, models
 import feedparser
--- dailyfeed/config.py
+++ dailyfeed/config.py
@@ -1,12 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '666b601b6739add4b3c04df94d9fe4f1'
-
-    db_path = os.path.join(os.path.dirname(__file__), 'app.db')
-    db_uri = 'sqlite:///{}'.format(db_path)
-    SQLALCHEMY_DATABASE_URI = db_uri
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp-mail.outlook.com'
     MAIL_PORT = 587
