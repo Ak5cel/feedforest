90425e532ed33dc340a3fb534f7eccdaba2bd4b9
Akhila Ficel <akhilaficel@gmail.com>
2020-07-08 19:57:26 +0100

    Added user_feed_map table

--- feedforest/models.py
+++ feedforest/models.py
@@ -42,11 +42,19 @@ class Article(db.Model):
         return f"Article('{self.title}', '{self.link}')"
 
 
+# Association table between users and followed feeds
+user_feed_map = db.Table('user_feed_map',
+                         db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                         db.Column('feed_id', db.Integer, db.ForeignKey('rss_feed.id'), primary_key=True))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    selected_feeds = db.relationship('RSSFeed', secondary=user_feed_map, lazy='subquery',
+                                     backref=db.backref('selected_by', lazy=True))
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
