1e3b0a0472abb55e5dd0a2f5ba5fef48f590f33f
Akhila Ficel <akhilaficel@gmail.com>
2020-07-12 04:57:33 +0100

    Edited topics dropdown to display active selection

--- feedforest/static/filter-scripts.js
+++ feedforest/static/filter-scripts.js
@@ -79,12 +79,22 @@ $(document).ready(function() {
     $('.topic-selector').on('click', (e) => {
         let topic = e.target.id;
         if (topic == 'all') {
+            // Show all feeds
             $('.feed-info').show().addClass('d-flex');
+            // Set dropdown to display 'All Topics'
+            $('#topicsDropdown').text('All Topics');
         } else {
-            // Hide all feeds
+            // Hide all feeds:
+            // Removing 'd-flex' is necessary as otherwise the !important property of
+            // Bootstrap d-flex overrides hide()
             $('.feed-info').hide().removeClass('d-flex');
             // Show just the feeds from the selected topic
-            $('.feed-info' + '.' + topic).show().addClass('d-flex');    
+            $('.feed-info' + '.' + topic).show().addClass('d-flex');   
+            // Set dropdown to display the selected topic
+            $('#topicsDropdown').text(topic);
         }
+        // Highlight selected topic in the dropdown-menu
+        $('.topic-selector').removeClass('active');
+        $(e.target).addClass("active");
     })
 })
