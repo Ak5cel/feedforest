c3671178dd680d44d8bd3058ef3ae36a1153fd45
Akhila <akhilaficel@gmail.com>
2020-08-03 05:09:37 +0000

    Get json config file using env variable

--- src/config.py
+++ src/config.py
@@ -1,15 +1,20 @@
 import os
+import json
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+CONFIG_FILE_PATH = os.environ.get('FEEDFOREST_CONFIG_FILE_PATH')
+
+with open(CONFIG_FILE_PATH) as config_file:
+    config = json.load(config_file)
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '666b601b6739add4b3c04df94d9fe4f1'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SECRET_KEY = config.get('SECRET_KEY') or '666b601b6739add4b3c04df94d9fe4f1'
+    SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = config.get('MAIL_SERVER')
+    MAIL_PORT = config.get('MAIL_PORT')
+    MAIL_USE_TLS = config.get('MAIL_USE_TLS')
+    MAIL_USERNAME = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD = config.get('MAIL_PASSWORD')
