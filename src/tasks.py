2085f3901200e9bbf299e0045515d810aaf417e3
Akhila <akhilaficel@gmail.com>
2020-08-05 02:19:40 +0000

    Update celerybeat schedule

--- src/tasks.py
+++ src/tasks.py
@@ -24,14 +24,14 @@ def setup_periodic_tasks(sender, **kwargs):
 
     # Calls fetch_articles() every hour
     sender.add_periodic_task(
-        crontab(minute='*/5'),
+        crontab(minute=0, hour='*/1'),
         fetch_articles.s()
     )
 
     # Sends daily emails to those users whose chosen email time lies
     # in the next ten minutes
     sender.add_periodic_task(
-        crontab(minute='*/5'),
+        crontab(minute='*/10'),
         send_batch_emails_handler.s()
     )
 
