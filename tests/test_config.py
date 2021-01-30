a892f1a4cd8bfe8f95d3153114e11756bf70aa5b
Akhila Ficel <akhilaficel@gmail.com>
2021-01-30 11:57:59 +0000

    Remove Config class inheritance TestConfig
    Earlier, the `TestConfig` class extended the `Config` class, but this caused
    problems while running Travis builds as the `Config` class sets some values
    from an on-system json file. Making TestConfig standalone solves this issue.
    
    Might need to implement a better fix later on, but this works for now.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,17 +1,17 @@
-from src.config import Config
-
-
-class TestConfig(Config):
+class TestConfig():
 
     # Enable the TESTING flag to disable the error catching during request handling
     # so that you get better error reports when performing test requests against the application.
     TESTING = True
+
+    SECRET_KEY = '666b601b6739add4b3c04df94d9fe4f1'
 
     # Disable CSRF tokens in the Forms (only valid for testing purposes!)
     WTF_CSRF_ENABLED = False
 
     # Use an in-memory sqlite database
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # + join(_cwd, 'testing.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Bcrypt algorithm hashing rounds (reduced for testing purposes only!)
     BCRYPT_LOG_ROUNDS = 4
